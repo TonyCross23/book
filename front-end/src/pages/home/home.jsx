e0f62f715fa117b5cfbc0a4adba2f6546a8f48e7
TonyCross23 <tonychamp40@gmail.com>
2025-01-24 20:06:16 +0630

    home

--- front-end/src/pages/home/home.jsx
+++ front-end/src/pages/home/home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Banner from "./banner";
 import TopSeller from "./topSeller";
+import Recommened from "./recommened";
+import News from "./news";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <TopSeller />
+      <Recommened />
+      <News />
     </>
   );
 };
