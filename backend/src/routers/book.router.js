fcd9c4e59b948b8045b813fa7c26a2645c8fd006
TonyCross23 <tonychamp40@gmail.com>
2025-02-03 20:25:35 +0630

    middleware

--- backend/src/routers/book.router.js
+++ backend/src/routers/book.router.js
@@ -6,13 +6,14 @@ import {
   getBookById,
   updateBook,
 } from "../controllers/book.controller.js";
+import verifyAdminToken from "../middleware/verifyAdminToken.js";
 
 const bookRouter = express.Router();
 
-bookRouter.post("/", bookCreate);
+bookRouter.post("/", verifyAdminToken, bookCreate);
 bookRouter.get("/", getAllBooks);
 bookRouter.get("/:id", getBookById);
-bookRouter.put("/:id", updateBook);
-bookRouter.delete("/:id", deleteBook);
+bookRouter.put("/:id", verifyAdminToken, updateBook);
+bookRouter.delete("/:id", verifyAdminToken, deleteBook);
 
 export default bookRouter;
